b1eb2dc4a33b0d12058c5e4f64bb301f1f5e374d
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-01-30 11:09:14 +0545

    Add files via upload

---
+++ LINKEDBST.cpp
@@ -0,0 +1,31 @@
+#include "LINKEDBST.h"
+#include "BST.h"
+#include<iostream>
+using namespace std;
+	
+LinkedBST::LinkedBST(){
+}
+LinkedBST::~LinkedBST(){
+}
+
+/*void LinkedBST::add(Node* root,int data){
+	cout<<"Hello"<<endl;
+	
+}*/
+
+void LinkedBST::preorder_traversal(){
+	cout<<"Hello"<<endl;
+}
+
+/*bool LinkedBST::search(int data){
+	cout<<"Hello"<<endl;
+}*/
+
+int main(){
+	LinkedBST A;
+	A.preorder_traversal();
+	return 0;
+}
+
+
+
