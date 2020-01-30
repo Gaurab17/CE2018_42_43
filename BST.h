b5bb00e75a71d830afb1223987c709ba79916d27
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-01-30 10:21:15 +0545

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class BST{
+
+	public:
+		virtual void add(int data)=0;
+		virtual bool search(int data)=0;
+		virtual void preorder_traversal()=0;
+
+
+};
+
