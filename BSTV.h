53d4c772193797d0d09a8ab9eefc0a5bacb435d9
AronShrestha <52240049+AronShrestha@users.noreply.github.com>
2020-02-05 20:49:50 +0545

    Update BSTV.h

--- BSTV.h
+++ BSTV.h
@@ -4,8 +4,9 @@ class BST{
 
 	public:
 		virtual void add(int data)=0;
-		virtual int search(int data)=0;
-		virtual void preorder_traversal()=0;
+		//virtual bool search(int data,int node=1)=0;
+		virtual bool search(int data)=0;
+		virtual void preorder_traversal(int index=1)=0;
 
 
 };
