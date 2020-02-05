79d8f1131f23ca18e02a8b2aba04243ddaa30763
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-02-05 22:35:22 +0545

    Update and rename BST.h to BinaryTree.h

---
+++ BinaryTree.h
@@ -0,0 +1,11 @@
+#ifndef BINARYTREE_h
+#define BINARYTREE_h
+
+
+class BinarySearchTree{
+	public:
+		virtual void preordertraversal()=0;
+		virtual void add(int data)=0;
+		virtual bool search(int data)=0;
+};
+#endif
