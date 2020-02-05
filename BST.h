bb16157d4648a6b516a8f946e84524ba48882f87
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-02-05 07:29:50 +0545

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,12 +1,13 @@
 #include<iostream>
+#ifndef BINARYTREE_h
+#define BINARYTREE_h
 using namespace std;
-class BST{
+
+class BinarySearchTree{
 
 	public:
-		virtual void add(int data)=0;
-		virtual bool search(int data)=0;
-		virtual void preorder_traversal()=0;
-
-
+		//virtual void add(int data)=0;
+		 bool search(int data)=0;
+		//virtual void preorder_traversal()=0;
 };
-
+#endif
