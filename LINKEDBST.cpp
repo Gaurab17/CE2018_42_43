d54086cbe649a4a4ded6ace249cc108be7f569a2
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-02-06 09:53:09 +0545

    Update LINKEDBST.cpp

--- LINKEDBST.cpp
+++ LINKEDBST.cpp
@@ -1,5 +1,6 @@
+#include"linkedlistBST.h"
 #include<iostream>
-#include"LinkedBST.h"
+
 using namespace std;
 
 node::node(){
@@ -13,87 +14,108 @@ node::node(int value){
 }
 
 node::~node(){}
-void LinkedBST::add(node *root,int data){
-	if(root -> data ==0){
-		root -> data = data;
-		cout<<data<<endl;
-	}
-	else{
-		if(data < root->data){
-			if(!root ->left){
-				node *newnode= new node(data);
-				root -> left = newnode;
-			}
-			else
-				add(root->left, data);
-		}
-		else if(data > root -> data){
-			if(!root ->right){
-				node *newnode= new node(data);
-				root -> right = newnode;
-			}
-			else 
-				add(root -> right, data);
-				cout<<data<<endl;
-		}
-		
-	}
-	
+
+
+LinkedBST::LinkedBST(){
 }
 
-bool LinkedBST::search(node *root,int data){
-	if(root->data==0)
-		cout<<"The Tree is empty.So search fail"<<endl;
-	else{
-		node *p = new node();
-		p = root;
-		while(p){
-			if(data >  p -> data)
-				p = p->right;
-			else if(data < p->data)
-				p = p->left;
-			else if (data == p->data){
-				cout<<"Search Completed! "<<data <<"was found in the tree"<<endl;
-				return true;
-		}
-			else{
-				cout<<data<<"wasn't found in the tree"<<endl;
-				return false;
-			}
-				
-					
-		}
-	}
-	cout<<data<<"is not inside the tree"<<endl;
-	return false;
+LinkedBST::~LinkedBST(){}
+
+void LinkedBST::add(int data){
 }
-/*void LinkedBST::preorderTraversal(node *root,int data) { 
+
+void LinkedBST::preorderTraversal(){}
+
+bool LinkedBST::search(int data){}
+
+
+void LinkedBST::add(node *root,int data){
+if (root->data==0){
+    root->data=data;
+}else{
+        if (data < root->data){
+            if (!root->left){ 
+                node *newnode=new node(data); 
+                root->left=newnode;
+            }
+            else{
+                add(root->left,data);
+            }
+        }
+        else if (data > root->data){
+            if (!root->right){ 
+                node *newnode=new node(data); 
+                root->right=newnode;
+            }
+            else{
+                add(root->right,data);
+            }
+        }
+    }
+}
+
+bool LinkedBST::search(node *root,int targetKey){
+    if(root->data==0){
+        cout<<"It is an empty tree"<<endl;
+    }
+    else{
+        node *p=new node();
+        p=root;
+        while(p){
+            if(targetKey>p->data){
+                p=p->right;
+            }
+            else if(targetKey<p->data){
+                p=p->left;
+            }
+            else if(targetKey==p->data){
+                cout<<targetKey<<" is in the tree"<<endl;
+                return 1;
+                
+            }
+            else{
+                cout<<targetKey<<" is not in the tree"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<targetKey<<" is not in the tree"<<endl;
+    return 0;
+}
+
+
+void LinkedBST::preorderTraversal(node* root) { 
     if (!root) 
-    return;    
-    cout << root->data << "\t";   
+    return; 
+  
+    
+    cout << root->data << "\t"; 
+  
+    
     preorderTraversal(root->left);  
+  
+    
     preorderTraversal(root->right); 
 }  
-*/
-int main(){
-	LinkedBST *a;
-	a->add(&a->root,6);
-	a->add(&a->root,4);
-	a->add(&a->root,7);
-	a->add(&a->root,2);
-	
-/*	cout<<"Preorder Traversal of the tree is"<<endl;
-	a.preordertraversal(&a.root);
-	cout<<endl;
-*/
-	int number;
-	char character;
-	search:
-	cout<<"Enter any number you want to search for in the tree"<<endl;
-	cin>>number;
-	a->search(&a->root,number);
-	cout<<"Do you want to search again(y/n)??"<<endl;
-	cin>>character;
-	if((character=='y') || (character=='Y')) goto search;
-}
 
+
+int main(){
+	LinkedBST s;
+
+	s.add(&s.root,46);
+	s.add(&s.root,19);
+	s.add(&s.root,77);
+	s.add(&s.root,33);
+	s.add(&s.root,81);
+	s.add(&s.root,51);
+	cout<<"Preorder Traversal of the tree is"<<endl;
+	s.preorderTraversal(&s.root);
+	cout<<endl;
+	
+	
+	int number;
+	cout<<"Enter a number you want to search  in the tree"<<endl;
+	cin>>number;
+	s.search(&s.root,number);
+	
+}
