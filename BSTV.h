3c939fcb27c46d1a2db22834f3147732e54128f0
Gaurab17 <55453709+Gaurab17@users.noreply.github.com>
2020-02-08 19:28:39 +0545

    Update BSTV.h

--- BSTV.h
+++ BSTV.h
@@ -1,13 +1,19 @@
-#include<iostream>
-using namespace std;
-class BST{
+#ifndef BINARYTREE_h
+#define BINARYTREE_h
 
+
+class BST{
 	public:
 		virtual void add(int data)=0;
-		//virtual bool search(int data,int node=1)=0;
-		virtual bool search(int data)=0;
-		virtual void preorder_traversal(int index=1)=0;
-		virtual int min()=0;
 
+		virtual bool search(int data)=0;
+
+        virtual void preordertraversal()=0;
+        virtual void inordertraversal() = 0;
+        virtual void deleteitem(int data) = 0;
+
+		virtual int maximum() = 0;
+		virtual int minimum() = 0;
 
 };
+#endif
