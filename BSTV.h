2d486cc1199d843d6b283f3dde8604fbcc271c92
AronShrestha <aronshrestha498@gmail.com>
2020-02-09 22:42:43 +0545

    Final change

--- BSTV.h
+++ BSTV.h
@@ -5,9 +5,16 @@ class BST{
 	public:
 		virtual void add(int data)=0;
 		//virtual bool search(int data,int node=1)=0;
-		virtual bool search(int data)=0;
+		virtual int search(int data)=0;
 		virtual void preorder_traversal(int index=1)=0;
+		virtual void inorder_traversal(int index=1)=0;
+		virtual int left_child(int index)=0;
+		virtual int right_child(int index)=0;
+
 		virtual int min()=0;
+
+		virtual int max()=0;
+
 
 
 };
